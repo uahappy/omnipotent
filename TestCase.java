f7723bc961f30afec7a77f2b6e82f5300c65190e
wzy <765208390@qq.com>
2022-05-28 14:39:17 +0800

    Revert "新增a变量"
    This reverts commit 0307b9bc9a7de47b482f4fc92980fe297ac0205c.

--- TestCase.java
+++ TestCase.java
@@ -1,25 +1,6 @@
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
-public class TestCase implements Serializable {
-    private static final long serialVersionUID = 6438268415057497151L;
-
-
-
+public class TestCase {
     public static void main(String[] args) {
-
-        int a = 1;
-
-
-        int i =1;
-        for (i = 1; i < 100; i++) {
-            System.out.println(i+1);
-        }
-        String str = "hello world";
-        List llll = new ArrayList<String>();
-        System.out.println(llll);
-
+        String s = "hello world";
+        System.out.println("hello world");
     }
-
 }
