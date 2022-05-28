76af1cc7cdbee6f116afdda69b4dc0040f23f2a9
wzy <765208390@qq.com>
2022-05-28 14:10:01 +0800

    cherry-pick dev的"新增a变量"提交

--- TestCase.java
+++ TestCase.java
@@ -1,6 +1,25 @@
-public class TestCase {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestCase implements Serializable {
+    private static final long serialVersionUID = 6438268415057497151L;
+
+
+
     public static void main(String[] args) {
-        String s = "hello world";
-        System.out.println("hello world");
+
+        int a = 1;
+
+
+        int i =1;
+        for (i = 1; i < 100; i++) {
+            System.out.println(i+1);
+        }
+        String str = "hello world";
+        List llll = new ArrayList<String>();
+        System.out.println(llll);
+
     }
+
 }
